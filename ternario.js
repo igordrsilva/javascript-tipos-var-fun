6557e61ba5de15ca11f381eddb55414f4da70a24
igordrsilva <igordrsilva@gmail.com>
2023-03-22 20:26:49 -0300

    tipos-var-fun

---
+++ ternario.js
@@ -0,0 +1,10 @@
+const idadeMinima = 18
+const idadeCliente = 16
+
+//if (idadeMinima <= idadeCliente) {
+//    console.log('Cerveja')
+//} else {
+//    console.log('Suco')}
+
+// Operador ternário
+console.log(idadeCliente >= idadeMinima ? 'Cerveja' : 'Suco')
