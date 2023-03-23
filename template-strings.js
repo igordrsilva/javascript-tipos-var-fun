a2ba4e7b7b29e028bd22a912406cf5cfcfdb0647
igordrsilva <igordrsilva@gmail.com>
2023-03-22 21:01:37 -0300

    template-string

---
+++ template-strings.js
@@ -0,0 +1,5 @@
+const nome = 'Ígor'
+const idade = 2023-2003
+const cidadeDeNascimento = 'Lajedo'
+const apresentacao = `Meu nome é ${nome}, tenho ${idade} e nasci na cidade de ${cidadeDeNascimento}`
+console.log(apresentacao)
